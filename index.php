155fbcb73478cc7413453e2d691431a8bc3fdb27
JeanReisCard <63821090+JeanReisCard@users.noreply.github.com>
2020-04-24 18:38:57 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html ng-app="ass">
+	<head>
+		<meta charset="utf-8">
+		<title>Assinatura de E-mail </title>
+		<script src="js/jquery-2.2.3.js"></script>
+		<script src="js/angular.min.js"></script>
+		<script src="js/atakAssinaturaAngula.js"></script>
+		<script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="css/atakAssinatura.css">
+
+
+	</head>
+	<body>
+		<div id="divPai"   ng-controller="assCtrl">
+
+			<section class="jumbotron">
+				
+				<img src="imagens/logo.png">
+			</section>
+
+			<section id="faixa1">
+			<footer>
+				<p colspan='2' align='right'><font face=Calibri color=white size=2><em>
+				</em> </font></p>
+			</footer>
+				
+			</section>
+
+
+			<section id="sectionForm">
+				<form id="formDados" method="get" action="/atakarejo/criaImagem1.php" >
+					<label id="labelNome">Nome:</label>
+					<input type="text" name="nome" ng-model="nome" placeholder="Nome"  id="nome"  > <br>
+
+					<label>Setor:</label>
+					<input type="text" name="cargo" ng-model="cargo" placeholder="Setor"><br>
+
+					<label>E-mail: </label>
+					<input type="email" name="email" ng-model="email" placeholder="E-mail"><br>
+
+					<label id="labelTelefone">Skype:</label>
+					<input type="email" name="skype" ng-model="skype" placeholder="skype@cardexpressbrasil.com.br"><br>
+
+					<label>Celular:</label>
+					<input type="tel" name="cel" ng-model="cel" placeholder=" (11) 9 0000-0000 "><br>
+                    
+                    <label id="labelTelefone">Telefone:</label>
+					<input type="tel" name="tel" ng-model="tel" placeholder="(11) 9 0000-0000 "><br>
+                    
+                    <label id="labelNome">Filial:</label>
+					<input type="text" name="filial" ng-model="filial" placeholder="São Paulo - SP"  id="filial"> 
+
+					<button class="btn btn-primary"  class="small" id="btnCriarAssi" type="submit" ng-disabled="!nome || !cargo || !email || !skype" ! ng-click="limpaDados()" >Gerar Assinatura</button>
+				</form> 
+				
+			</section>
+			<section id="sectionBtnPasso">
+				
+			<p colspan='2' align='left'><font face=Calibri color=#000 size=2><a href="assSuporte.php"><img src="imagens/btn02.png"></a><em>  Clique aqui e saiba como configurar sua nova assinatura!
+				</em> </font></p>
+			</section>
+
+		</div>
+		
+	</body>
+
+</html>
